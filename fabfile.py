fbd5e7ed6d045ea035c921045f6d175a06c52ffc
Mustafa Khattab <mustafa.khattab@gmail.com>
2012-04-12 14:29:07 +0200

    adding fabric deploy task

--- fabfile.py
+++ fabfile.py
@@ -6,8 +6,16 @@ from fabric.context_managers import prefix
 
 env.use_ssh_config = True
 env.hosts = ["odesk-aws"]
-PROJECT_DIR = os.path.join(os.path.dirname(__file__), "project")
+
+LOCAL_PROJECT_DIR = os.path.join(os.path.dirname(__file__), "project")
+REMOTE_PROJECT_DIR = "/home/ubuntu/candidates_emailer"
 
 
-def test():
-    run("echo $PWD")
+def deploy():
+    with cd(LOCAL_PROJECT_DIR):
+        local("git push")
+    
+    with prefix("source {0}".format(os.path.join(
+        REMOTE_PROJECT_DIR, "..", ".virtualenvs", "odesk", "bin", "activate"))):
+        with cd(REMOTE_PROJECT_DIR):
+            run("git pull")
