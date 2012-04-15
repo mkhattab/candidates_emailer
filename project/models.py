7e465b9a0af0896418fc5b0cdb51f4cebf771139
Mustafa Khattab <mustafa.khattab@gmail.com>
2012-04-15 22:52:01 +0200

    Adding User model and database settings. Not tracking db.

---
+++ project/models.py
@@ -0,0 +1,29 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+
+from app import app
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    access_token = db.Column(db.String)
+    access_token_secret = db.Column(db.String)
+
+    def __init__(self, email, first_name,
+                 last_name, access_token, access_token_secret):
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.access_token = access_token
+        self.access_token_secret = self.access_token_secret
+
+    def __repr__(self):
+        return "<User {0}>".format(self.email)
+
+    @property
+    def full_name(self):
+        return "{0} {1}".format(self.first_name, self.last_name)
--- project/settings.py
+++ project/settings.py
@@ -1,3 +1,10 @@
 #Project settings
+import os
+
+PROJECT_DIR = os.path.dirname(__file__)
+def _project_path(*dirs):
+    return os.path.join(PROJECT_DIR, *dirs)
+
+SQLALCHEMY_DATABASE_URI = "sqlite:///{0}".format(_project_path("dev.db"))
 
 from odesk_settings import *
