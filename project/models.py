32c74db6e37c62240ee2f5824dbdd5fb007194a5
Mustafa Khattab <mustafa.khattab@gmail.com>
2012-04-16 15:45:24 +0200

    Adding alternate email field (plus other fields) to User model

--- project/models.py
+++ project/models.py
@@ -8,18 +8,29 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, unique=True)
+    alternate_email = db.Column(db.String)
+    use_alternate_email = db.Column(db.Boolean, nullable=False)
+    send_email = db.Column(db.Boolean, nullable=False)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
     access_token = db.Column(db.String)
     access_token_secret = db.Column(db.String)
 
-    def __init__(self, email, first_name,
-                 last_name, access_token, access_token_secret):
-        self.email = email
-        self.first_name = first_name
-        self.last_name = last_name
-        self.access_token = access_token
-        self.access_token_secret = access_token_secret
+    def __init__(self, *args, **kwargs):
+        self.email = kwargs.get("email")
+        self.first_name = kwargs.get("first_name")
+        self.last_name = kwargs.get("last_name")
+        self.access_token = kwargs.get("access_token")
+        self.access_token_secret = kwargs.get("access_token_secret")
+        self.alternate_email = kwargs.get("alternate_email")
+        self.use_alternate_email = kwargs.get("use_alternate_email")
+        self.send_email = kwargs.get("send_email")
+        
+        if not self.use_alternate_email:
+            self.use_alternate_email = False
+
+        if not self.send_email:
+            self.send_email = True
 
     def __repr__(self):
         return "<User {0}>".format(self.email)
