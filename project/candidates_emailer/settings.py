7ac5bf38052f4d0f852bfb7608d4ef2dd9208c59
Mustafa Khattab <mustafa.khattab@gmail.com>
2012-04-17 17:53:31 +0200

    Fix: not using _project_path to set db path

--- project/candidates_emailer/settings.py
+++ project/candidates_emailer/settings.py
@@ -1,10 +1,6 @@
 #Project settings
 import os
 
-PROJECT_DIR = os.path.dirname(__file__)
-def _project_path(*dirs):
-    return os.path.join(PROJECT_DIR, *dirs)
-
-SQLALCHEMY_DATABASE_URI = "sqlite:///{0}".format(_project_path("dev.db"))
+SQLALCHEMY_DATABASE_URI = "sqlite:///dev.db"
 
 from candidates_emailer.odesk_settings import *
