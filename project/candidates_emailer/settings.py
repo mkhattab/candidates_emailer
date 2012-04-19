36dffbef6e1af9117bbb95a6bbd706a2302341ad
Mustafa Khattab <mustafa.khattab@gmail.com>
2012-04-19 09:36:15 +0200

    Storing local configuration in local_settings module

--- project/candidates_emailer/settings.py
+++ project/candidates_emailer/settings.py
@@ -1,6 +1,12 @@
 #Project settings
 import os
+import sys
 
 SQLALCHEMY_DATABASE_URI = "sqlite:///dev.db"
 
 from candidates_emailer.odesk_settings import *
+
+try:
+    from candidates_emailer.local_settings import *
+except ImportError:
+    print >> sys.stderr, "No local_settings.py found!"
